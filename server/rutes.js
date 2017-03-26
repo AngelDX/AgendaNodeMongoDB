1a0e4ea18e3da1ed7217e44ea6f7355cec87b2c5
Angel R. Condori C <angel.condoro@gmail.com>
2017-03-26 17:19:14 -0500

    Nuevas funcionalidades

--- server/rutes.js
+++ server/rutes.js
@@ -10,7 +10,7 @@ Router.get('/all',function(req,res){
 		}
 		res.json(docs)
 	})
-});
+})
 
 Router.get('/cargar_eventos',function(req,res){
 	Eventos.find({},{"_id":0,"title":1,"start":1,"end":1}).exec(function(err,docs){
@@ -20,25 +20,8 @@ Router.get('/cargar_eventos',function(req,res){
 		}
 		res.json(docs)
 	})
-});
-
-Router.post('/new',function(req,res){
-	let user=new Users({
-		userid:Math.floor(Math.random()*50),
-		nombres:req.body.nombres,
-		apellidos: req.body.apellidos,
-		edad: req.body.edad,
-		sexo: req.body.sexo,
-		estado: "Activo"
-	})
-	user.save(function(error){
-		if(error){
-			res.status(500)
-			res.json(error)
-		}
-		res.send("Registro Guardado")
-	})
 })
+
 
 Router.post('/login',function(req,res){
 	Users.find({usuario:req.body.user,password:req.body.pass}, function(err, doc){
@@ -71,6 +54,30 @@ Router.post('/agregar_evento',function(req,res){
 	})
 })
 
+Router.post('/eliminar_evento:id',function(req,res){
+	let uid = req.params.id
+    Users.remove({id: uid}, function(error) {
+        if(error) {
+            res.status(500)
+            res.json(error)
+        }
+        res.send("Registro eliminado")
+    })
+})
 
+Router.put('/actulizar_evento:id',function(req,res){
+	let evento=new Eventos({
+		title:req.body.titulo,
+		start:req.body.fechai,
+		end:req.body.fechaf
+	})
+	evento.update(function(error){
+		if(error){
+			res.status(500)
+			res.json(error)
+		}
+		res.send("Registro Guardado")
+	})
+})
 
 module.exports=Router
